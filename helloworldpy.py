014c50233ccee4eab43fb2e55a8045c6e8285b3f
Marco Bedeschi <MBedeschi@jmawireless.com>
2024-05-17 15:34:24 +0200

    fine prima sessione

--- helloworldpy.py
+++ helloworldpy.py
@@ -5,3 +5,13 @@ print('Ciao,', c)
 
 nome = input('inserisci nome: ')
 print("hai inserito ", nome)
+
+x = 10
+x = "python"
+x = [1, 2, 3]
+print(x)
+
+s = 'python, python, pithon!'
+
+print(s.replace('thon', 'py', 1))
+
